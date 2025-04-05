c7ab19e8afcce20131e6e9422ce4e3b56281fb6b
Samarth-170904 <samarthmule1704@gmail.com>
2025-04-05 22:50:18 +0530

    Crawled all the data in SHLTask1.csv and Gemini API is saved in dotenv

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import streamlit as st
+import google.generativeai as genai
+import pandas as pd
+from dotenv import load_dotenv
+import json
+
+# Load API key securely
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+# Configure Gemini API
+genai.configure(api_key=GEMINI_API_KEY)
+
+# Load SHL Assessments Dataset
+DATA_FILE = "SHLTask1.csv"
+df = pd.read_csv(DATA_FILE)
+
